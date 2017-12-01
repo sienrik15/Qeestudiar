baa4965a36e38bbb7dd949bdb2bddbe749c7801e
sienrik15 <enrique_sam11@hotmail.com>
2017-12-01 00:44:48 -0500

    perfil detail activity

---
+++ app/src/main/java/com/example/user/qeestudiar/DetailPerfilActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.qeestudiar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_perfil);
+    }
+}
